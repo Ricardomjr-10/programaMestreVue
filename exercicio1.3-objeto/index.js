361f7ff930ec884f5116214ddcf39684c2f6be7b
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-23 20:26:55 -0300

    criado objeto pessoa

---
+++ exercicio1.3-objeto/index.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome:'Ricardo',
+    idade: '40',
+    saudar: () => {
+        console.log('Olá')
+    }
+}
+
+console.log(pessoa)
+console.log(pessoa.nome)
+console.log(pessoa.idade)
+pessoa.saudar()
