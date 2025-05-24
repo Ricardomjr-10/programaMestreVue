a77510c5ed0c7609a95979435b97aaa6dabaff92
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-23 21:16:06 -0300

    array destructuring

--- exercicio1.6-destructuring/index.js
+++ exercicio1.6-destructuring/index.js
@@ -17,3 +17,19 @@ const [f1,f2, ...restFruits] = fruits
 console.log(f1)
 console.log(f2)
 console.log(restFruits)
+
+//default values
+
+const array = []
+const [val1 = 10, val2 = 20] = array
+
+console.log(val1, val2)
+
+//swapping variables
+
+let x = 5
+let y = 10
+
+[x, y] = [y, x]
+
+console.log(x, y)
