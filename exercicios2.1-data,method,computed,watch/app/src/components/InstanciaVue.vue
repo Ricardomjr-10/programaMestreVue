47895c43e10aaef314eef500b4948624b436a413
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-26 21:54:14 -0300

    instacia methods

--- exercicios2.1-data,method,computed,watch/app/src/components/InstanciaVue.vue
+++ exercicios2.1-data,method,computed,watch/app/src/components/InstanciaVue.vue
@@ -2,6 +2,9 @@
     <div>
         <h1>Instancias Vue</h1>
         <p>{{ nome }}</p>
+        <p>{{ contador }}<button @click="incrementar">Incrementar</button></p>
+        <p>{{ estaAtivo }}</p>
+        <p>{{ listadeItes }}</p>
     </div>
 </template>
 
@@ -9,8 +12,17 @@
 export default {
     data() {
          return {
-            nome:'Ricardo'
+            nome:'Ricardo',
+            contador:0,
+            estaAtivo: false,
+            listadeItes: [1, 2, 3]
          }
+    },
+
+    methods: {
+        incrementar() {
+            this.contador++
+        }
     }
 }
 </script>
