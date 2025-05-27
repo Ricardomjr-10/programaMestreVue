5d3d53d0ccd553bb22d23e3613efc8a04b4dbf6d
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-26 22:08:35 -0300

    instacia methods1

--- exercicios2.1-data,method,computed,watch/app/src/components/InstanciaVue.vue
+++ exercicios2.1-data,method,computed,watch/app/src/components/InstanciaVue.vue
@@ -2,7 +2,11 @@
     <div>
         <h1>Instancias Vue</h1>
         <p>{{ nome }}</p>
-        <p>{{ contador }}<button @click="incrementar">Incrementar</button></p>
+        <p>{{ contador }}</p>
+        <p>{{ mensagem }}</p>
+        <button @click="incrementar">Incrementar</button>
+        <button @click="decrementar">Decrementar</button>
+        <button @click="somar(10)">Somar 10</button>
         <p>{{ estaAtivo }}</p>
         <p>{{ listadeItes }}</p>
     </div>
@@ -15,13 +19,32 @@ export default {
             nome:'Ricardo',
             contador:0,
             estaAtivo: false,
-            listadeItes: [1, 2, 3]
+            listadeItes: [1, 2, 3],
+            mensagem:''
          }
     },
 
     methods: {
         incrementar() {
             this.contador++
+            this.atualizarMensagem()
+        },
+
+        decrementar() {
+            this.contador--
+            this.atualizarMensagem()
+        },
+
+        atualizarMensagem() {
+            if (this.contador > 0) {
+                this.mensagem = `O contador está em ${this.contador}`
+            } else {
+                this.mensagem = `O contador está zerado ou negativo`
+            }
+        },
+
+        somar(valor) {
+            this.contador += valor
         }
     }
 }
