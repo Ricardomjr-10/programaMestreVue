d0fe8a8b4d80f51bead3dce06773fc02ba0e5196
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-30 20:35:45 -0300

    diretivas

---
+++ exercicios2.1-data,method,computed,watch/app/src/components/DiretivasIf.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>V-if, v-for e v-on</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
