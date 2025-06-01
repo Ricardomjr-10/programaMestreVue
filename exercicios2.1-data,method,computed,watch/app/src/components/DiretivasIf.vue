52fecb4bc940cc580491cc7bb9bb30b48a4603b6
Ricardo-10 <ricardomjr2@gmail.com>
2025-05-31 21:50:09 -0300

    v-model checbox

--- exercicios2.1-data,method,computed,watch/app/src/components/DiretivasIf.vue
+++ exercicios2.1-data,method,computed,watch/app/src/components/DiretivasIf.vue
@@ -23,38 +23,50 @@
             <p v-if="isAdmin">Você tem acesso total.</p>
             <button v-if="showButton">Clique aqui</button>
         </div>
-            <p v-for="numero in numeros" :key="numero">{{ numero }}</p>
-            <p v-for="nome in nomes" :key="nome">{{ nome }}</p>
+        <p v-for="numero in numeros" :key="numero">{{ numero }}</p>
+        <p v-for="nome in nomes" :key="nome">{{ nome }}</p>
+        <div>
+            <ul>
+                <li v-for="(fruta, index) in frutas" :key="index">{{ fruta }}</li>
+            </ul>
+            <ul>
+                <li v-for="pessoa in pessoas" :key="pessoa.id">
+                    {{ pessoa.nome }} ({{ pessoa.idade }} anos)
+                </li>
+            </ul>
             <div>
+                <h4>Lista de compras</h4>
                 <ul>
-                    <li v-for="(fruta, index) in frutas" :key="index">{{ fruta }}</li>
-                </ul>
-                <ul>
-                    <li v-for="pessoa in pessoas" :key="pessoa.id">
-                        {{ pessoa.nome }} ({{ pessoa.idade }} anos)
+                    <li v-for="(item, index) in lista" :key="index">
+                        {{ index + 1 }}.{{ item }}
                     </li>
                 </ul>
                 <div>
-                    <h4>Lista de compras</h4>
+                    <h4>usuario</h4>
                     <ul>
-                        <li v-for="(item, index) in lista" :key="index">
-                            {{ index + 1 }}.{{ item }}
+                        <li v-for="(value, key, index,) in usuario" :key="index">
+                            {{ index + 1 }}. {{ key }}: {{ value }}
                         </li>
                     </ul>
-                    <div>
-                        <h4>usuario</h4>
-                        <ul>
-                            <li v-for="(value, key, index,) in usuario" :key="index">
-                                {{ index + 1 }}. {{ key }}: {{ value }}
-                            </li>
-                        </ul>
-                    </div>
                 </div>
             </div>
-            <div>
-                <h4>Teste lista numerica</h4>
-                <span v-for="n in 5" :key="n"> {{ n }} <br></span>
-            </div>
+        </div>
+        <div>
+            <h4>Teste lista numerica</h4>
+            <span v-for="n in 5" :key="n"> {{ n }} <br></span>
+        </div>
+        <div>
+            <label for="mensagem">Sua Mensagem:</label><br>
+            <textarea id="mensagem" v-model="minhaMensagem"></textarea>
+            <p>Você digitou: {{ minhaMensagem }}</p>
+        </div>
+        <div>
+            <h4>check box</h4>
+            <input type="checkbox" id="aceitarTermos" v-model="aceitoTermos">
+            <label for="aceitarTermos">Eu concordo com os termos e condições.</label>
+            <p v-if="aceitoTermos">Obrigado por aceitar!</p>
+            <p v-else>Por favor, aceite os termos para continuar.</p>
+        </div>
     </div>
 </template>
 
@@ -62,6 +74,8 @@
 export default {
     data() {
         return {
+            aceitoTermos:false,
+            minhaMensagem: 'Escreva aqui',
             nome: '',
             paragrafo: {
                 color: 'red',
@@ -72,24 +86,24 @@ export default {
             isLoggedIn: true,
             isAdmin: true,
             showButton: true,
-            numeros:[1,2,3,4,5],
+            numeros: [1, 2, 3, 4, 5],
             nomes: {
-                1:'um',
-                2:'dois',
-                3:'tres'
+                1: 'um',
+                2: 'dois',
+                3: 'tres'
             },
             frutas: ['maça', 'uva', 'laranja', 'goiaba'],
             pessoas: [
-                {id:1, nome: 'joao', idade: 26},
-                {id:2, nome: 'roberto', idade: 30},
-                {id:3, nome: 'carla', idade: 18}
+                { id: 1, nome: 'joao', idade: 26 },
+                { id: 2, nome: 'roberto', idade: 30 },
+                { id: 3, nome: 'carla', idade: 18 }
             ],
-            lista: ['leite', 'pao','ovo'],
+            lista: ['leite', 'pao', 'ovo'],
 
             usuario: {
-                nome:'carlos',
-                sobrenome:'rodrigues',
-                idade:40
+                nome: 'carlos',
+                sobrenome: 'rodrigues',
+                idade: 40
             }
         }
     },
