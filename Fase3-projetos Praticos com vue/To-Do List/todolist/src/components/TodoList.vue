9125bc3e32281675d29bf15e7177da680989e142
Ricardo-10 <ricardomjr2@gmail.com>
2025-06-04 20:25:20 -0300

    commit initial

---
+++ Fase3-projetos Praticos com vue/To-Do List/todolist/src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Simples lista de tarefas</h1>
+        <label for="tarefa"></label>
+        <input type="text" v-model="minhaTarefa" placeholder="digite sua tarefa">
+        <button @click="adicionarTarefa">Adicionar Tarefa</button>
+        <p>{{ tarefas }}</p>
+        <ul>
+            <li v-for="tarefa in tarefas" :key="tarefa">{{ tarefa }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+    const minhaTarefa = ref('')
+    const tarefas = ref([])
+
+    const adicionarTarefa = () => {
+        tarefas.value.push(minhaTarefa)
+    }
+</script>
