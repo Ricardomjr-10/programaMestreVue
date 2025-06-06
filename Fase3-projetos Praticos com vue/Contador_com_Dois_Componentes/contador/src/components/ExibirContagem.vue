e439d67dfdebe268e7a43866695d70bffebccc8a
Ricardo-10 <ricardomjr2@gmail.com>
2025-06-05 21:20:01 -0300

    commit initial

---
+++ Fase3-projetos Praticos com vue/Contador_com_Dois_Componentes/contador/src/components/ExibirContagem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Contador</h1>
+        <div>{{ numero }}</div>
+        <button @click="aumentar">Aumentar</button>
+        <button @click="diminuir">Diminnuir</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const numero = ref(0)
+
+const aumentar = () => {
+    numero.value++
+}
+
+const diminuir = () => {
+    numero.value--
+}
+</script>
