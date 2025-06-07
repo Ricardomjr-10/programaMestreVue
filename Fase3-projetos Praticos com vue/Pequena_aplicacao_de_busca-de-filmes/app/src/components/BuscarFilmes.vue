2feb06d0035683d9b507e1d470b8131ac1d3650e
Ricardo-10 <ricardomjr2@gmail.com>
2025-06-07 19:54:58 -0300

    commit initial

---
+++ Fase3-projetos Praticos com vue/Pequena_aplicacao_de_busca-de-filmes/app/src/components/BuscarFilmes.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Pesquise um Filme</h1>
+        <input type="text" placeholder="Digite o nome do filme">
+        <div>
+            <div>Nome do Filme</div>
+            <img src="" alt="poster">
+        </div>
+    </div>
+</template>
