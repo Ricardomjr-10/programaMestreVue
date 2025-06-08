c7a020b6c5113d39cc4d4c12ff437b8e35397f54
Ricardo-10 <ricardomjr2@gmail.com>
2025-06-07 21:16:13 -0300

    buscando poster

--- Fase3-projetos Praticos com vue/Pequena_aplicacao_de_busca-de-filmes/app/src/components/BuscarFilmes.vue
+++ Fase3-projetos Praticos com vue/Pequena_aplicacao_de_busca-de-filmes/app/src/components/BuscarFilmes.vue
@@ -4,23 +4,33 @@
         <input type="text" v-model="titleFilme" placeholder="Digite o nome do filme">
         <button @click="pesquisarFilme">Buscar</button>
         <div>
-            <div>Nome do Filme</div>
-            <img src="" alt="poster">
+            <div>Nome do Filme: {{ user.Title }}</div>
+            <img src={{ user.Poster }} alt="poster">
         </div>
     </div>
 </template>
 
+
 <script setup>
-import { ref } from 'vue';
+import { ref} from 'vue'
 
-
+const user = ref('')
 const titleFilme = ref('')
 const apiKey = ref('49e27f1e')
 
-const url = fetch(`http://www.omdbapi.com/?t=${titleFilme.value}&apikey=${apiKey.value}`)
 
-const pesquisarFilme = () => {
+const pesquisarFilme = async () => {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey.value}&t=${titleFilme.value}`)
+    
+    const data = await response.json()
+    
+    user.value = data
 
-    console.log(url)
+    console.log(user.value)
 }
+
+
+
+
+
 </script>
